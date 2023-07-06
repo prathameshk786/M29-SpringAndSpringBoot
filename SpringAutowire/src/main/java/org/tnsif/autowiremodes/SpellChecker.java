c871154d392fc239802d84de3bc51fdf03603fc4
2144AX <2144AX@Prathamesh>
2023-07-06 21:34:34 +0530

    Commit

---
+++ SpringAutowire/src/main/java/org/tnsif/autowiremodes/SpellChecker.java
@@ -0,0 +1,11 @@
+package org.tnsif.autowiremodes;
+
+public class SpellChecker {
+	public SpellChecker() {
+		System.out.println("Spell Checker Constructor");
+	}
+	public void checkSpelling() {
+		System.out.println("Chceking the spelling and correcting it");
+	}
+
+}
---
+++ SpringAutowire/src/main/java/org/tnsif/autowiremodes/TextEditor.java
@@ -0,0 +1,29 @@
+package org.tnsif.autowiremodes;
+
+public class TextEditor {
+	//DI in the form of objects
+	private SpellChecker checker;
+	//DI in the form of literals
+	private String name;
+
+	public SpellChecker getChecker() {
+		return checker;
+	}
+
+	public void setChecker(SpellChecker checker) {
+		this.checker = checker;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void display() {
+		checker.checkSpelling();
+	}
+
+}
